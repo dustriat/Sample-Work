231d8705bd5d386cd2d335057b49ffe522f00160
Dustin <dustriat@gmail.com>
2026-02-25 14:21:47 -0700

    Lyft Trip Data Queries.sql

---
+++ Codecademy SQL/Lyft Trip Data Queries.sql
@@ -0,0 +1,60 @@
+/*
+This query shows that trip 1004
+had a rider with id 105, but 105 is
+not shown in the rider table
+*/
+SELECT * FROM trips;
+SELECT * FROM riders;
+SELECT * FROM cars;
+
+SELECT *
+FROM riders
+CROSS JOIN cars;
+
+--Trip log with users
+SELECT *
+FROM trips
+LEFT JOIN riders
+ON trips.rider_id = riders.id;
+/*
+This query shows that trip 1004
+had a rider with id 105, but 105 is
+not shown in the rider table
+*/
+
+--Cars used on each trip
+SELECT *
+FROM trips
+INNER JOIN cars
+ON trips.car_id = cars.id;
+
+--Show first table and secondary table with new riders
+SELECT *
+FROM riders
+UNION
+SELECT *
+FROM riders2;
+
+--Find the average cost per trip
+SELECT AVG(cost)
+FROM trips;
+
+--Email campaign needs all riders with fewer than 500 trips
+SELECT *
+FROM riders
+WHERE total_trips < 500
+UNION
+SELECT *
+FROM riders2
+WHERE total_trips < 500;
+
+--Number of cars with active status
+SELECT COUNT(*)
+FROM cars
+WHERE status = 'active';
+
+--Identify which 2 cars have the most trips to schedule their service
+SELECT *
+FROM cars
+ORDER BY trips_completed DESC
+LIMIT 2;
