5b031ce9d7c1214cf9da34374938280842403562
Dustin <dustriat@gmail.com>
2026-03-03 11:44:39 -0700

    Update

---
+++ Codecademy Python/Block Letters.py
@@ -0,0 +1,8 @@
+# Dustin Riat
+print("DDDD "," ","RRRR ")
+print("D   D"," ","R   R")
+print("D   D"," ","R   R")
+print("D   D"," ","RRRR ")
+print("D   D"," ","R R  ")
+print("D   D"," ","R  R ")
+print("DDDD "," ","R   R")
